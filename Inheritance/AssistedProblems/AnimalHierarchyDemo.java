b29c8591141fad59fd8459fdf2cf8f1af1ce1bfc
SiddhiDublish22 <siddhidublish.22@gmail.com>
2025-09-18 10:59:22 +0530

    [Siddhi] added animal hierarchy code

---
+++ Inheritance/AssistedProblems/AnimalHierarchyDemo.java
@@ -0,0 +1,62 @@
+package Inheritance.AssistedProblems;
+
+class Animal {
+    protected String name;
+    protected int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void makeSound() {
+        System.out.println("Animal makes a sound.");
+    }
+}
+
+class Dog extends Animal {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " barks: Woof! Woof!");
+    }
+}
+
+class Cat extends Animal {
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " meows: Meow! Meow!");
+    }
+}
+
+class Bird extends Animal {
+    public Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " chirps: Tweet! Tweet!");
+    }
+}
+
+public class AnimalHierarchyDemo {
+    public static void main(String[] args) {
+        Animal a1 = new Dog("Buddy", 3);
+        Animal a2 = new Cat("Whiskers", 2);
+        Animal a3 = new Bird("Tweety", 1);
+
+        a1.makeSound();
+        a2.makeSound();
+        a3.makeSound();
+    }
+}
+
+
