7e87441526f21a3279c043c655e5d0e7861815bf
SiddhiDublish22 <siddhidublish.22@gmail.com>
2025-09-18 11:26:26 +0530

    [Siddhi] added library management code

---
+++ Inheritance/SingleInheritance/LibraryManagement.java
@@ -0,0 +1,48 @@
+package Inheritance.SingleInheritance;
+
+class Book {
+    protected String title;
+    protected int publicationYear;
+
+    public Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Publication Year: " + publicationYear);
+    }
+}
+
+class Author extends Book {
+    private String name;
+    private String bio;
+
+    public Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author: " + name);
+        System.out.println("Bio: " + bio);
+    }
+}
+
+public class LibraryManagement {
+    public static void main(String[] args) {
+        Author authorBook = new Author(
+            "Java Programming",
+            2022,
+            "John Doe",
+            "An experienced Java developer and instructor."
+        );
+        authorBook.displayInfo();
+    }
+}
+
+
