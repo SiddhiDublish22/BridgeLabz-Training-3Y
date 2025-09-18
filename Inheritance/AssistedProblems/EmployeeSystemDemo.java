7ad4ec0ce9cd5b9929a02cb21ceab1b1d5e65f53
SiddhiDublish22 <siddhidublish.22@gmail.com>
2025-09-18 11:04:41 +0530

    [Siddhi] added employee management system code

---
+++ Inheritance/AssistedProblems/EmployeeSystemDemo.java
@@ -0,0 +1,76 @@
+package Inheritance.AssistedProblems;
+
+// Employee Management System
+class Employee {
+    protected String name;
+    protected int id;
+    protected double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name + ", ID: " + id + ", Salary: " + salary);
+    }
+}
+
+class Manager extends Employee {
+    private int teamSize;
+
+    public Manager(String name, int id, double salary, int teamSize) {
+        super(name, id, salary);
+        this.teamSize = teamSize;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Team Size: " + teamSize);
+    }
+}
+
+class Developer extends Employee {
+    private String programmingLanguage;
+
+    public Developer(String name, int id, double salary, String programmingLanguage) {
+        super(name, id, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Programming Language: " + programmingLanguage);
+    }
+}
+
+class Intern extends Employee {
+    private String mentorName;
+
+    public Intern(String name, int id, double salary, String mentorName) {
+        super(name, id, salary);
+        this.mentorName = mentorName;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Mentor: " + mentorName);
+    }
+}
+
+public class EmployeeSystemDemo {
+    public static void main(String[] args) {
+        Employee e1 = new Manager("Alice", 101, 90000, 5);
+        Employee e2 = new Developer("Bob", 102, 60000, "Java");
+        Employee e3 = new Intern("Charlie", 103, 20000, "Alice");
+
+        e1.displayDetails();
+        e2.displayDetails();
+        e3.displayDetails();
+    }
+}
+
